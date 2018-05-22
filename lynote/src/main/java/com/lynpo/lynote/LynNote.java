353d9cc797ad47f60986814f591a2446359db20b
fujw <fujw@dxyer.com>
2018-05-22 10:46:55 +0800

    Complete exercise code ENV-params.

--- lynote/src/main/java/com/lynpo/lynote/LynNote.java
+++ lynote/src/main/java/com/lynpo/lynote/LynNote.java
@@ -1,4 +1,14 @@
 package com.lynpo.lynote;
 
+/**
+ * All run result in this module is build with the java version:
+ *
+ * $ java -version
+ * java version "1.8.0_161"
+ * Java(TM) SE Runtime Environment (build 1.8.0_161-b12)
+ * Java HotSpot(TM) 64-Bit Server VM (build 25.161-b12, mixed mode)
+ */
 public class LynNote {
+
+    public static final String HELLO_WORLD = "Hello World! This is Lynote";
 }
--- lynote/src/main/java/com/lynpo/lynote/oom/JavaVMStackSOF.java
+++ lynote/src/main/java/com/lynpo/lynote/oom/JavaVMStackSOF.java
@@ -6,6 +6,11 @@ package com.lynpo.lynote.oom;
  * JavaVMStackSOF
  *
  * VM Args: -Xss128k
+ *
+ * $ java -version
+ * java version "1.8.0_161"
+ * Java(TM) SE Runtime Environment (build 1.8.0_161-b12)
+ * Java HotSpot(TM) 64-Bit Server VM (build 25.161-b12, mixed mode)
  */
 public class JavaVMStackSOF {
 
@@ -24,5 +29,21 @@ public class JavaVMStackSOF {
             System.out.println("stack length:" + oom.stackLength);
             throw e;
         }
+
+        // run-result(default):
+        // stack length:16338
+        // Exception in thread "main" java.lang.StackOverflowError
+        //	at com.lynpo.lynote.oom.JavaVMStackSOF.stackLeak(JavaVMStackSOF.java:17)
+        //	at com.lynpo.lynote.oom.JavaVMStackSOF.stackLeak(JavaVMStackSOF.java:17)
+
+        // configs: -Xss128k
+        // The stack size specified is too small, Specify at least 160k
+        // Error: Could not create the Java Virtual Machine.
+        // Error: A fatal exception has occurred. Program will exit.
+
+        // configs: -Xss256k
+        // Exception in thread "main" java.lang.StackOverflowError
+        // stack length:2449
+        //	at com.lynpo.lynote.oom.JavaVMStackSOF.stackLeak(JavaVMStackSOF.java:20)
     }
 }

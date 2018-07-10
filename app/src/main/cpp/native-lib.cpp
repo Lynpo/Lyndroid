929fcc23874df2b7f5ce822ad67fcc4a2cc3f7ed
fujw <fujw@dxyer.com>
2018-07-10 11:40:33 +0800

    cpp file bug fix.

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -4,22 +4,27 @@
 
 const std::string Lynpo_Const::LYNPO_NAME = "Lynpo";
 
+bool valid(int a, int b){
+    return a > b;
+}
+
+//extern "C"
+
+bool packageValid(JNIEnv *env, jobject ctx){
+    jclass android_content_Context =env->GetObjectClass(ctx);
+    jmethodID midGetPackageName = env->GetMethodID(android_content_Context,"getPackageName", "()Ljava/lang/String;");
+    jstring packageName= (jstring)env->CallObjectMethod(ctx, midGetPackageName);
+
+    jstring real_pkg_name = env->NewStringUTF("com.lynpo");
+
+    jclass cls = env->GetObjectClass(real_pkg_name);
+    jmethodID mID = env->GetMethodID(cls, "equals", "(Ljava/lang/Object;)Z");
+    jboolean equals = env->CallBooleanMethod(real_pkg_name, mID, packageName);
+
+    return equals;
+}
+
 extern "C"
-
-//bool packageValid(JNIEnv *env, jobject ctx){
-//    jclass android_content_Context =env->GetObjectClass(ctx);
-//    jmethodID midGetPackageName = env->GetMethodID(android_content_Context,"getPackageName", "()Ljava/lang/String;");
-//    jstring packageName= (jstring)env->CallObjectMethod(ctx, midGetPackageName);
-//
-//    jstring real_pkg_name = env->NewStringUTF("com.lynpo");
-//
-//    jclass cls = env->GetObjectClass(real_pkg_name);
-//    jmethodID mID = env->GetMethodID(cls, "equals", "(Ljava/lang/Object;)Z");
-//    jboolean equals = env->CallBooleanMethod(real_pkg_name, mID, packageName);
-//
-//    return equals;
-//}
-
 JNIEXPORT jstring JNICALL Java_com_lynpo_jniinvoke_JniVisitor_stringFromJNI(
         JNIEnv *env,
         jclass,
@@ -28,11 +33,13 @@ JNIEXPORT jstring JNICALL Java_com_lynpo_jniinvoke_JniVisitor_stringFromJNI(
     std::string name = Lynpo_Const::LYNPO_NAME;
     std::string sentence = hello.append(name);
 
-    return env->NewStringUTF(sentence.c_str());
-//    if (packageValid(env, obj)) {
-//        return env->NewStringUTF(sentence.c_str());
-////        return env->NewStringUTF(hello.c_str());
-//    } else {
-//        return env->NewStringUTF("A u kidding me $_$");
-//    }
+    int a = 3, b = 2;
+    if (valid(a, b)) {
+        sentence.append(", \nNo kidding");
+    }
+    if (packageValid(env, obj)) {
+        return env->NewStringUTF(sentence.c_str());
+    } else {
+        return env->NewStringUTF("A u kidding me $_$");
+    }
 }

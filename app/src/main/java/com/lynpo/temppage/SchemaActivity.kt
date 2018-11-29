a9018e059b232f474a05017db8f2e32a2f94bd3f
fujw <fujw@dxyer.com>
2018-11-29 16:52:42 +0800

    Add Uri test.

--- app/src/main/java/com/lynpo/temppage/SchemaActivity.kt
+++ app/src/main/java/com/lynpo/temppage/SchemaActivity.kt
@@ -3,6 +3,7 @@ package com.lynpo.temppage
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import com.lynpo.R
 import com.lynpo.base.BaseActivity
 import kotlinx.android.synthetic.main.activity_schema.*
@@ -30,5 +31,13 @@ class SchemaActivity : BaseActivity() {
             intent.data = uri
             startActivity(intent)
         }
+
+        val url1 = "http://lynpo.com/path/name/id/121"
+        val uri = Uri.parse(url1)
+        Log.d("debug_tag", "uri:schema:" + uri.scheme)  // http
+        Log.d("debug_tag", "uri:host:" + uri.host)  // lynpo.com
+        Log.d("debug_tag", "uri:path:" + uri.path)  // /path/name/id/121
+        Log.d("debug_tag", "uri:pathSegments:" + uri.pathSegments)  // [path, name, id, 121]
+        Log.d("debug_tag", "uri:lastPathSegment:" + uri.lastPathSegment)    // 121
     }
 }

d325d31303d3326fabf3ededa894c22dea9ed0ce
fujw <fujw@dxyer.com>
2019-05-16 16:43:57 +0800

    animation sample update.

--- app/src/main/java/com/lynpo/view/animation/AnimationActivity.kt
+++ app/src/main/java/com/lynpo/view/animation/AnimationActivity.kt
@@ -40,7 +40,6 @@ class AnimationActivity : BaseActivity() {
         super.onResume()
 
         displayColorAnimation()
-        displayMultiAnimation()
     }
 
     private fun displayColorAnimation() {
@@ -57,14 +56,18 @@ class AnimationActivity : BaseActivity() {
     private fun displayMultiAnimation() {
         val animSet = AnimatorSet()
         animSet.playTogether(
-                ObjectAnimator.ofFloat(tvAnimationSet, "rotateX", 0f, 360f),
-                ObjectAnimator.ofFloat(tvAnimationSet, "rotateY", 0f, 180f),
-                ObjectAnimator.ofFloat(tvAnimationSet, "rotation", 0f, -720f),
-                ObjectAnimator.ofFloat(tvAnimationSet, "translationX", 0f, 90f),
-                ObjectAnimator.ofFloat(tvAnimationSet, "translationY", 0f, 90f),
-                ObjectAnimator.ofFloat(tvAnimationSet, "scaleX", 1f, 1.5f),
-                ObjectAnimator.ofFloat(tvAnimationSet, "scaleY", 1f, 0.5f),
-                ObjectAnimator.ofFloat(tvAnimationSet, "alpha", 1f, 0.25f, 1f)
+//                ObjectAnimator.ofFloat(tvAnimationSet, "rotateX", 0f, 360f),
+//                ObjectAnimator.ofFloat(tvAnimationSet, "rotateY", 0f, 180f),
+//                ObjectAnimator.ofFloat(tvAnimationSet, "rotation", 0f, -720f),
+//                ObjectAnimator.ofFloat(tvAnimationSet, "translationX", 0f, 90f),
+//                ObjectAnimator.ofFloat(tvAnimationSet, "translationY", 0f, 90f),
+//                ObjectAnimator.ofFloat(tvAnimationSet, "scaleX", 1f, 1.5f),
+//                ObjectAnimator.ofFloat(tvAnimationSet, "scaleY", 1f, 0.5f),
+//                ObjectAnimator.ofFloat(tvAnimationSet, "alpha", 1f, 0.25f, 1f),
+                ObjectAnimator.ofFloat(tvAnimation, "translationX", 0f, 90f, 0f, 100f, 0f, 150f, 0f, 200f),
+                ObjectAnimator.ofFloat(tvAnimation, "translationY", 0f, 400f),
+                ObjectAnimator.ofFloat(tvAnimationSet, "translationX", 0f, 90f, 0f, 100f, 0f, 150f, 0f, 200f),
+                ObjectAnimator.ofFloat(tvAnimationSet, "translationY", 0f, 400f)
         )
 
         animSet.setDuration(5 * 1000).start()
